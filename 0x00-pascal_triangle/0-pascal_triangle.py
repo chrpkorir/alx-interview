dd1b5056306a9173b943f8d9022c2a1f2b409f4c
Chrpkorir <faythkoko@gmail.com>
2022-12-22 13:36:43 +0000

    Update pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,6 +2,8 @@
 """
 A module for Pascal's triangle.
 """
+
+
 def pascal_triangle(n):
     """ Returns a list of lists of ints representing Pascal's triangle of n """
     
@@ -16,15 +18,14 @@ def pascal_triangle(n):
         left = -1
         right = 0
         in_pas = []
-
-    for j in range(i + 1):
-        num = 0
-        if left > -1:
-            num += a[i - 1][left]
-        if right < i:
-            num += pascal[i - 1][right]
-        left += 1
-        right += 1
-        in_pas.append(num)
-    pascal.append(in_pas)
+        for j in range(i + 1):
+            num = 0
+            if left > -1:
+                num += a[i - 1][left]
+            if right < i:
+                num += pascal[i - 1][right]
+            left += 1
+            right += 1
+            in_pas.append(num)
+        pascal.append(in_pas)
     return pascal
