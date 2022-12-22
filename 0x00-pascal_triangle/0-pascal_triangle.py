98e87f628b497018015d67c8c857d3d57cd801b8
Chrpkorir <faythkoko@gmail.com>
2022-12-22 13:25:27 +0000

    Update pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -8,14 +8,23 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    a = [[] for i in range(n)]
-    for i in range(n):
-        for j in range(i + 1):
-            if j < i:
-                if j == 0:
-                    a[i].append(i)
-                else:
-                    a[i].append(a[i - 1][j - 1])
-            elif j == i:
-                a[i].append(1)
-    return a
+    pascal = [[1]]
+    if n == 1:
+        return pascal
+
+    for i in range(1, n):
+        left = -1
+        right = 0
+        in_pas = []
+
+    for j in range(i + 1):
+        num = 0
+        if left > -1:
+            num += a[i - 1][left]
+        if right < i:
+            num += pascal[i - 1][right]
+        left += 1
+        right += 1
+        in_pas.append(num)
+    pascal.append(in_pas)
+    return pascal
